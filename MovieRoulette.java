e7ade28b21bd87e151e3772e81e60dd275fd0b80
Looty9397 <naphtali.rutzky@gmail.com>
2026-09-15 17:00:32 +0000

    Add all java files

---
+++ MovieRoulette.java
@@ -0,0 +1,7 @@
+
+
+public class MovieRoulette {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ Roulette.java
@@ -0,0 +1,8 @@
+public class Roulette {
+    public static Movie chooseOne (Movie[] list) {
+        return new Movie();
+    }
+
+    public static Movie[] chooseX (Movie[] list) {
+    }
+}
