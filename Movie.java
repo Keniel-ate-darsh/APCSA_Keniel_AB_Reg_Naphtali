8f6a6b033396566a38992758da450c74e305d509
Looty9397 <naphtali.rutzky@gmail.com>
2026-09-15 17:13:15 +0000

    Finish Movie.java

--- Movie.java
+++ Movie.java
@@ -1,5 +1,67 @@
 public class Movie {
-    public Movie () {
-        
+    // Attributes
+    private String genre;
+    private int year;
+    private int runtime;
+    private double rating;
+    private String title;
+
+    // Constructor
+    public Movie (String genre, int year, int runtime, double rating, String title) {
+        this.genre = genre;
+        this.year = year;
+        this.runtime = runtime;
+        this.rating = rating;
+        this.title = title;
+    }
+
+    // Accesssors and Mutators
+    public String getGenre () {
+        return this.genre;
+    }
+
+    public void setGenre (String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear () {
+        return this.year;
+    }
+
+    public void setYear (int year) {
+        this.year = year;
+    }
+
+    public int getRuntime () {
+        return this.runtime;
+    }
+
+    public void setRuntime (int runtime) {
+        this.runtime = runtime;
+    }
+
+    public double getRating () {
+        return this.rating;
+    }
+
+    public void setRating (double rating) {
+        this.rating = rating;
+    }
+
+    public String getTitle () {
+        return this.title;
+    }
+
+    public void setTitle (String title) {
+        this.title = title;
+    }
+
+    // Other
+    public String toString () {
+        return "%s (%d, %s) %f★".formatted(this.title, this.year, this.getRuntimeString(), this.rating);
+    }
+
+    public String getRuntimeString () {
+        return "%dh %dm".formatted(this.runtime / 60, this.runtime % 60);
     }
 }
